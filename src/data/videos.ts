5e3cabfe2d7f81638cfd52020d33f90fb914ef51
adsterrabd622-dotcom <adsterrabd622@gmail.com>
2026-04-18 09:47:02 +0300

    Add files via upload

---
+++ src/data/videos.ts
@@ -0,0 +1,35 @@
+export interface Video {
+  id: string;
+  title: string;
+  thumbnail: string;
+  duration: string;
+  description: string;
+  videoUrl: string;
+}
+
+export const videos: Video[] = [
+  {
+    id: "1",
+    title: "Beautiful Nature Highlights",
+    thumbnail: "https://picsum.photos/seed/nature3/640/360",
+    duration: "10:05",
+    description: "A stunning collection of natural landscapes.",
+    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4"
+  },
+  {
+    id: "2",
+    title: "Urban Architecture",
+    thumbnail: "https://picsum.photos/seed/urban/640/360",
+    duration: "08:20",
+    description: "Exploring modern city design.",
+    videoUrl: "https://www.w3schools.com/html/movie.mp4"
+  },
+  {
+    id: "3",
+    title: "Culinary Arts",
+    thumbnail: "https://picsum.photos/seed/food/640/360",
+    duration: "12:15",
+    description: "Mastering the art of gourmet cooking.",
+    videoUrl: "https://www.w3schools.com/html/mov_bbb.mp4"
+  }
+];
